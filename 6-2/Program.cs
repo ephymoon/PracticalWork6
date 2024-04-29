3c12bb78d008ce1aa15ea690dec49576f0d323a2
Ephymoon <Ephymoon@DESKTOP-2811859>
2024-04-29 12:03:54 +1000

    Add project files.

---
+++ 6-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Программа нахождения максимального модуля числа из массива.\nВведите количество элементов массива:");
+            int k = Convert.ToInt32(Console.ReadLine());
+            int[] x = new int[k];
+            Random rnd = new Random();
+            Console.Write("Эллементы массива:\n");
+            for (int i = 0; i < k; i++)
+            {
+                x[i] = rnd.Next(-50,50);
+                Console.Write(x[i] + " ");
+
+            }
+            Console.WriteLine();
+            int max = 0;
+            for (int i = 0; i < k; i++)
+            {
+                if (Math.Abs(x[i]) > max) max = Math.Abs(x[i]);
+            }
+            Console.Write("Максимальное число в массиве по модулю: " + max);
+            Console.Write("\nДля завершения работы программы нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 6-3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Программа переворачивания массива.\nВведите количество элементов массива:");
+            int k = Convert.ToInt32(Console.ReadLine());
+            int[] x = new int[k];
+            int[] c = new int[k];
+            Random rnd = new Random();
+            Console.Write("Эллементы массива:\n");
+            for (int i = 0; i < k; i++)
+            {
+                x[i] = rnd.Next(-50, 50);
+                Console.Write(x[i] + " ");
+            }
+            c = x;
+            Array.Reverse(c);
+            Console.WriteLine();
+            Console.WriteLine("Массив c: ");
+            for (int i = 0; i < k; i++)
+            {
+                Console.Write(c[i] + " ");
+            }
+            Console.Write("\nДля завершения работы программы нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
+    
